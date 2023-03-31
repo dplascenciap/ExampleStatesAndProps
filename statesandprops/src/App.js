57b2b0bc44df93bf89396edccebf14d65380460c
dplascenciap <dplascenciap@gmail.com>
2023-03-31 19:58:37 -0400

    States working fine :)

--- statesandprops/src/App.js
+++ statesandprops/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './App.css';
 
 // Import components
@@ -6,16 +6,12 @@ import Nav from './components/Nav';
 
 import Tweets from './components/Tweets';
 
+/** The following code is an example of how to use props
+ * I'll comment it to start a new App() and demonstrate the use of states
+*/
+
+/*
 function App() {
-
-  /*
-  const tweetsArray = [
-    {name:'David', age:35, tweet:'this is a tweet'},
-    {name:'Account1', age:00, tweet:'of course!'},
-    {name:'John', age:15, tweet:'hello world'}
-  ]
-  */
-
   return (
   <div className="App">
     <h1>Hello React</h1>
@@ -23,6 +19,40 @@ function App() {
       <Nav />
       <Tweets />
     </div>
+  </div>
+  );
+}
+*/
+
+function App() {
+
+  const [counter, setCounter] = useState(0);
+  const [toggle, setToggle] = useState(false);
+
+  const incrementer = () => {
+    // Since we are using states, it not possible to use the line below:
+    // counter += 1;
+    // Instead, we use the function associated with the variable 'counter'.
+    // In other words, we use 'setCounter' to modify the variable 'counter'.
+    // We can use a function like the one shown below:
+    // setCounter((prev) => prev + 1)
+    // We can also use a function like:
+    setCounter(counter + 1)
+    console.log(counter);
+  };
+
+  const toggleClass = () => {
+    setToggle((prevState) => !prevState);
+  };
+
+  // Refer to App.css to see the definition of className="active"
+
+  return (
+  <div className="App">
+    <h1 className={toggle ? "active" : ""}>Hello React</h1>
+    <h2>Counter {counter}</h2>
+    <button onClick={incrementer}>Increment Counter</button>
+    <button onClick={toggleClass}>Toggle Active</button>
   </div>
   );
 }
